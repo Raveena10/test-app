8a22ab8edf00ba3c032f740f6cb6450d2bfddb10
raveena10 <raveenamanghnani4@gmail.com>
2022-04-22 07:44:35 +0530

    Add data updated

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Route,Switch} from "react-router-dom";
 import ContactForm from './Components/Contactform';
 import store from './Store';
 import { Provider } from "react-redux";
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <>
+    <Navbar/>
     <Provider store={store}>
       <BrowserRouter>
         <Switch>
